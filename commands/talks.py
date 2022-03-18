76335b9c06f8f67a347119528f3ab6394c0f7d8c
Robertoskb <97955337+Robertoskb@users.noreply.github.com>
2022-03-18 11:36:50 -0300

    Update talks.py

--- commands/talks.py
+++ commands/talks.py
@@ -20,38 +20,52 @@ class Talks(commands.Cog):
         
 
     async def Raply(self, message):
-        msgtype = self.MsgType(message)
-        events = self.Responses(message)
+        msgtype = self.MsgType(message) # Tipo da mensagem
+        events = self.Responses(message) # Dicionário de respostas
         
-        if msgtype in events:
+        if msgtype in events: 
+            # Se o tipo da mensagem mensagem estiver no dicionário respostas
+            # O bot vai responder de acordo com tipo da mensagem recebida
             await message.reply(events[msgtype], mention_author=False)
             
                 
-    def MsgType(self, message) -> str:
-        typesdict = self.BotDict('input')
+    def MsgType(self, message) -> str: 
+        # Verifica o tipo da mensagem 
+        # Ex: 'oie' é do tipo 'hi'
+        # Retorna o tipo 'hi' se a mensagem conter 'oie'
+        # Se o tipo não existir, uma string vazia é retornada
+
+        typesdict = self.BotDict('input') # Dicionário com os tipos de mensagens
+        # Os tipos são as chaves
+        # Cada tipo tem uma lista de mensagens correspondentes
 
         msg = message.content.lower()
         
         msgtype = ''
-        for k, v in typesdict.items():
+        for k, v in typesdict.items(): 
             for i in v:
                 if f'"{i}"' in msg or f"'{i}'"in msg: continue
-                if i in msg: msgtype = k
+                if i in msg: msgtype = k 
+                # Se a string da mensagem recebida conter uma string de uma chave 
+                # O tipo da mensagem terá o valor da chave 
+
 
         return msgtype
 
     
     def Responses(self, message) -> dict:
-        Dict = self.BotDict('responses') 
+        Dict = self.BotDict('responses') # Dicionário de respostas
 
         f = message.author.name
-        for k, v in Dict.items():
-            Dict[k] = random.choice(v).format(f)
+        for k, v in Dict.items(): 
+            # Converte a lista de respostas em uma resposta aleatória
+            Dict[k] = random.choice(v).format(f) 
 
         return Dict
 
     
     def BotDict(self, mode:str) -> dict:
+        # Retorna um dicinário específico de um json
         with open(os.path.join(sys.path[0],'dict.json'), encoding='utf-8') as j:
             Dict = json.load(j)
 
