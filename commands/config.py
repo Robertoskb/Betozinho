9832e3071ea80c705fe41aff50da4706dee9c89b
Robertoskb <97955337+Robertoskb@users.noreply.github.com>
2022-04-21 18:36:07 -0300

    :bug:

--- commands/config.py
+++ commands/config.py
@@ -9,7 +9,7 @@ class Config(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.command(name='talks', help='Ativar ou Desativar minhas respostas', description='on/off')
     async def talks(self, ctx, on_off: str = ''):
         if ctx.channel.type == discord.ChannelType.private:
@@ -22,22 +22,22 @@ class Config(commands.Cog):
         else:
             await self.update(ctx, f'talks = {dict(on=1, off=0)[on_off]}')
             response = f'As respostas foram {dict(on="**ativadas**", off="**desativadas**")[on_off]}'
-        
+
         await ctx.reply(response, mention_author=False)
-    
+
     @commands.command(name='biblev', help='Mudar linguagem da bíblia', description='sigla da linguagem')
-    async def biblev(self, ctx, version: str= ''):
+    async def biblev(self, ctx, version: str = ''):
         if ctx.channel.type == discord.ChannelType.private:
             return
 
         versions = ['NVI', 'RA', 'ACF', 'KJV', 'BBE', 'RVR', 'APEE']
         if not version.upper() in versions:
             response = f'Digite uma versão entre {", ".join(versions)}'
-        
+
         else:
             await self.update(ctx, f'biblelang = "{version.lower()}"')
             response = f'Versão redefinida para {version.upper()}'
-        
+
         await ctx.reply(response, mention_author=False)
 
     @commands.command(name='reset', help='Resetar as minhas configurações', description='Sem argumentos')
@@ -46,13 +46,13 @@ class Config(commands.Cog):
 
         response = 'Configurações redefinidas'
         await ctx.reply(response, mention_author=False)
-    
+
     @awaitable
     def reset_config(self, ctx):
         server = ServerSettings(ctx.guild.id)
         server.reset()
         server.cursor.close()
-    
+
     @awaitable
     def update(self, ctx, values):
         server = ServerSettings(ctx.guild.id)
@@ -61,9 +61,10 @@ class Config(commands.Cog):
 
     async def cog_command_error(self, ctx, error):
         response = 'Algum erro ao atualizar as configurações'
-        ctx.reply(response,mention_author=False)
+        await ctx.reply(response, mention_author=False)
 
         print(error)
+
 
 def setup(bot):
     bot.add_cog(Config(bot))
