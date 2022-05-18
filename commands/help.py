04d062eefa3a67c8a74dd6f000bcdffffb7a5cac
José Roberto <97955337+Robertoskb@users.noreply.github.com>
2022-05-18 07:21:32 -0300

    :art:

--- commands/help.py
+++ commands/help.py
@@ -12,13 +12,13 @@ class Help(commands.Cog):
         embed = discord.Embed(title='Comando de Ajuda ^^', color=0x00B115)
 
         if not command_name:
-            embed = self.commandList(embed)
+            embed = self.command_list(embed)
         else:
-            embed = self.especificCommand(embed, command_name.lower())
+            embed = self.specific_command(embed, command_name.lower())
 
         await ctx.channel.send(embed=embed)
 
-    def commandList(self, embed: discord.Embed) -> discord.Embed:
+    def command_list(self, embed: discord.Embed) -> discord.Embed:
         cogs = self.cogs()
         cogs.remove('Help')
 
@@ -28,7 +28,7 @@ class Help(commands.Cog):
 
         return embed
 
-    def especificCommand(self, embed: discord.Embed, command_name: str) -> discord.Embed:
+    def specific_command(self, embed: discord.Embed, command_name: str) -> discord.Embed:
         cogs = self.cogs()
         embed.title = ''
 
@@ -40,24 +40,24 @@ class Help(commands.Cog):
                     continue
 
                 if command.name == command_name.lower():
-                    commandhelp = f'**-{command.name}  {command.description}**'
-                    embed.add_field(name=cog, value=commandhelp, inline=False)
+                    command_help = f'**-{command.name}  {command.description}**'
+                    embed.add_field(name=cog, value=command_help, inline=False)
 
                     return embed
 
-        return self.especificCog(embed, command_name)
+        return self.specific_cog(embed, command_name)
 
-    def especificCog(self, embed: discord.Embed, cog_name: str) -> discord.Embed:
-        commandlist = ""
+    def specific_cog(self, embed: discord.Embed, cog_name: str) -> discord.Embed:
+        command_list = ""
         for command in self.bot.get_cog(cog_name.capitalize()).walk_commands():
             if command.hidden:
                 continue
             if command.parent is not None:
                 continue
 
-            commandlist += f'**-{command.name}**  *{command.help}*\n'
+            command_list += f'**-{command.name}**  *{command.help}*\n'
 
-        embed.add_field(name=cog_name.capitalize(), value=commandlist, inline=False)
+        embed.add_field(name=cog_name.capitalize(), value=command_list, inline=False)
         embed.add_field(name='Help', value='**-help [comando]**', inline=False)
 
         return embed
@@ -70,9 +70,8 @@ class Help(commands.Cog):
 
         return cogs
 
-    async def cog_command_error(self, _, __):
-        print(__)
-        pass
+    async def cog_command_error(self, ctx, _):
+        await self.help(ctx)
 
 
 def setup(bot):
